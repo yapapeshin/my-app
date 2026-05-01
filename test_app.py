d0108cdd0078a2ae34b2fa17fd95955c6e4ef5ee
yapapeshin <papeshin15@gmail.com>
2026-05-02 02:04:50 +0800

    Initial commit

---
+++ test_app.py
@@ -0,0 +1,11 @@
+import unittest
+from app import app
+
+class TestApp(unittest.TestCase):
+    def test_home(self):
+        with app.test_client() as c:
+            response = c.get('/')
+            self.assertEqual(response.data, b"Hello from CI/CD!")
+
+if __name__ == '__main__':
+    unittest.main()
